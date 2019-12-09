4a95e0c65ae9c534eced67941927b1b6465f9b70
Benjamin Garza <minibeniitec@gmail.com>
2019-12-09 12:15:28 -0800

    auto

--- msp.cpp
+++ msp.cpp
@@ -31,23 +31,28 @@ void printLetter(int i);
 void setSize(int *size, int graph[][5]);
 void setTrack(int size, bool *track);
 void mspPrim(int size, bool track[], int graph[][5], int i);
-void mspKrus(int size, bool track[], int graph[][5], int i);
+void mspKrus(int size, bool track[], int graph[][5]);
+bool testCycle(int graph[][5], int source, int dest);
+void addToGraph(int graph[][5], int source, int dest);
+
 
 /*         Main          */
 int main(int argc, char **argv) {
     int size = 5; 
     bool track[5];
 
+
+    cout << "Prim's" << endl;
     for(int i = 0; i < size; i++) {
 	setTrack(size, track);
         mspPrim(size, track, graph, i);
 	cout << endl;
     }
-
     cout << endl << endl;
 
-    //setTrack(size, track);
-    //mspKrus(size, track, graph, 0);
+    cout << "Kruskal's" << endl;
+    setTrack(size, track);
+    mspKrus(size, track, graph);
 
     return 0;
 }
@@ -71,7 +76,7 @@ void printLetter(int i) {
 
 //This function finds the msp via Prim's Algorithm
 void mspPrim(int size, bool track[], int graph[][5], int i) {
-    int index;
+    int index = 0;
     track[i] = true;
     printLetter(i);
     for(int j = 0; j < size; j++)
@@ -98,12 +103,54 @@ void mspPrim(int size, bool track[], int graph[][5], int i) {
 }
 
 //This function finds the msp via Kruskal's Algorithm
-void mspKrus(int size, bool track[], int graph[][5], int i) {
+void mspKrus(int size, bool track[], int graph[][5]) {
+    int graph2[][5] = {
+        { 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0 }
+    };
 
+    int table[][3] = {
+        { 0, 0, 0 },
+        { 0, 0, 0 },
+        { 0, 0, 0 },
+        { 0, 0, 0 },
+        { 0, 0, 0 },
+        { 0, 0, 0 },
+        { 0, 0, 0 },
+        { 0, 0, 0 },
+        { 0, 0, 0 }
+    };
 
+    setTable(graph, table);
+    addToGraph(graph2, source, dest);
+    for(int i = 1; i < 10; i++) {
+        source = table[i][1];
+        dest = table[i][2];
+        if((track[source] != track[dest]) || (!track[source] && !track[dest]))
+            if(testCycle(graph2, source, dest)) {
+                addToGraph(graph2, source, dest);
+                track[source] = true;
+                track[dest] = true;
+            }
+    }
 } 
 
 void setTrack(int size, bool *track) {
     for(int i = 0; i < size; i++)
 	track[i] = false;
+}
+
+bool testCycle(int graph[][5], int source, int dest) {
+    bool works = true;
+    bool track[] = {false, false, false, false, false};
+    int x;
+    for(int i = 0; i < 5; i++) { 
+        while(
+} 
+
+void addToGraph(int graph[][5], int source, int dest) {
+     
 }
